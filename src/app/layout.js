a6199eca5e2632986a50346b3aecfc8214848b95
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-11-07 22:00:02 +0000

    feat: improve root layout and main navigation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
-import Navigation from '@/app/_components/Navigation';
-import Logo from '@/app/_components/Logo';
+import Header from '@/app/_components/Header';
 
 import '@/app/_styles/globals.css';
 
@@ -20,12 +19,11 @@ export default function RootLayout({ children }) {
    return (
       <html lang="en">
          <body
-            className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-            <Logo />
-            <Navigation />
-            <main>{children}</main>
-
-            <footer>Copywright by the The Wild Oasis Retreat</footer>
+            className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+            <Header />
+            <div className="flex-1 px-8 py-12">
+               <main className="max-w-7xl mx-auto">{children}</main>
+            </div>
          </body>
       </html>
    );
