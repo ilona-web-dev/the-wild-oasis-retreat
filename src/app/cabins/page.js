6cab2e169cb2708d9273c2cf5311b1c910070636
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-11-13 15:29:52 +0000

    feat(cabins): add capacity filter for cabin list

--- src/app/cabins/page.js
+++ src/app/cabins/page.js
@@ -1,6 +1,7 @@
 import CabinList from '@/app/_components/CabinList';
 import { Suspense } from 'react';
 import Spinner from '@/app/_components/Spinner';
+import Filter from '@/app/_components/Filter';
 
 export const metadata = {
    title: 'Cabins',
@@ -8,7 +9,11 @@ export const metadata = {
 
 // export const revalidate = 3600;
 
-export default function Page() {
+export default async function Page({ searchParams }) {
+   const params = await searchParams;
+
+   const filter = params?.capacity ?? 'all';
+
    return (
       <div>
          <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +27,11 @@ export default function Page() {
             your own little home away from home. The perfect spot for a
             peaceful, calm vacation. Welcome to paradise!
          </p>
-         <Suspense fallback={<Spinner />}>
-            <CabinList />
+         <div className="flex justify-end mb-8">
+            <Filter />
+         </div>
+         <Suspense fallback={<Spinner />} key={filter}>
+            <CabinList filter={filter} />
          </Suspense>
       </div>
    );
