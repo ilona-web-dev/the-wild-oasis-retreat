3c2472970fca68dad0f86ce0c9bb99ac7622fae4
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-11-17 21:08:06 +0000

    feat(auth): add Google sign-in and login page

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,13 +1,16 @@
+import { auth } from '@/app/_lib/auth';
+
 export const metadata = {
    title: 'Guest area',
 };
 
-function Page() {
+export default async function Page() {
+   const session = await auth();
+   const firstName = session.user.name.split(' ').at(0);
+
    return (
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-         Welcome, Ilona
+         Welcome, {firstName}!
       </h2>
    );
 }
-
-export default Page;
