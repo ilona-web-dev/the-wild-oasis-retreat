b97fcd95c99b0b585bd74f6d805b335c8f85f095
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-11-19 17:16:48 +0000

    feat(reservations): add optimistic delete list

--- src/app/account/reservations/page.js
+++ src/app/account/reservations/page.js
@@ -1,7 +1,7 @@
-import ReservationCard from '@/app/_components/ReservationCard';
 import Link from 'next/link';
 import { auth } from '@/app/_lib/auth';
 import { getBookings } from '@/app/_lib/data-service';
+import ReservationList from '@/app/_components/ReservationList';
 
 export default async function Page() {
    const session = await auth();
@@ -21,11 +21,7 @@ export default async function Page() {
                </Link>
             </p>
          ) : (
-            <ul className="space-y-6">
-               {bookings.map((booking) => (
-                  <ReservationCard booking={booking} key={booking.id} />
-               ))}
-            </ul>
+            <ReservationList bookings={bookings} />
          )}
       </div>
    );
